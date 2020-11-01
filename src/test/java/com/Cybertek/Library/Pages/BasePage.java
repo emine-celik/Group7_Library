b11cd9a67a911ec34f96fab8f257fbfc28b4e48d
iToniLo17 <toni31790@gmail.com>
2020-11-01 13:53:16 -0500

    added Driver, added ConfigurationReader, added configuration.properties, added dependencies

---
+++ src/test/java/com/Cybertek/Library/Pages/BasePage.java
@@ -0,0 +1,15 @@
+package com.Cybertek.Library.Pages;
+
+import com.Cybertek.Library.Utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+
+public class BasePage {
+
+
+    public BasePage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+
+
+}
